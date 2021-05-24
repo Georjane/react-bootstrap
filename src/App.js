cedec6c580749396da1318d45e558c55b168ea0d
Witah Georjane <pzgeorjane@gmail.com>
2021-05-24 05:58:15 +0100

    Bootstrap working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className="w-25 bg-warning">Make Responsive this Page</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+// import './custom.scss';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 ReactDOM.render(
   <React.StrictMode>
